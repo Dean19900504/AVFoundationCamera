018097a26250654be895dc79837e088b66a8be74
huangjy <huangjy@huangjymatoMacBook-Pro.local>
2012-07-03 11:36:32 +0800

    Initial Commit

---
+++ AVFoundationCamera/AVFoundationCameraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AVFoundationCameraAppDelegate.h
+//  AVFoundationCamera
+//
+//  Created by huangjy on 12-7-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVFoundationCameraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AVFoundationCamera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AVFoundationCamera
+//
+//  Created by huangjy on 12-7-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AVFoundationCameraAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AVFoundationCameraAppDelegate class]));
+    }
+}
